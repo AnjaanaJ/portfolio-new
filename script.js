6323027c641882842b5abe4e755b16bd24b4525d
AnjaanaJ <anjanajanani6@gmail.com>
2026-06-28 21:58:40 +0530

    stored frquently used elements

---
+++ script.js
@@ -0,0 +1,23 @@
+const body = document.body;
+
+const navbar =
+document.getElementById("navbar");
+
+const toggle =
+document.getElementById("themeToggle");
+
+const nav =
+document.getElementById("navLinks");
+
+const mobile =
+document.getElementById("mobileToggle");
+
+const form =
+document.getElementById("contactForm");
+
+const success =
+document.getElementById("formSuccess");
+
+const typing =
+document.getElementById("typing");
+
